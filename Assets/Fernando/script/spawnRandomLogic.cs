0b8a2b83304f3ce309d1e1d4af4c2853b0e25e8e
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-04-03 17:59:11 -0300

    Funcion aleatoria
    he intento de avance en para que sea ajustable

--- Assets/Fernando/script/spawnRandomLogic.cs
+++ Assets/Fernando/script/spawnRandomLogic.cs
@@ -1,37 +1,54 @@
+
 using UnityEngine;
 
 public class spawnRandomLogic : MonoBehaviour
 {
     public GameObject[] objectForTable;
-    public int[] space; // menor numer = menor tamaño igual en grande
+    //public int space; 
     public Transform tablePos;
-    public GameObject[,] squares = new GameObject[8, 8];
-    void Start()
+    public GameObject[,] squares = new GameObject[10, 10];
+    private int activatedX,activatedY;
+    //private bool available;
+    //private Dictionary<bool, GameObject[,]> availablePos;
+    private void Awake()
     {
+        // convendría crear una lista  sobre los numeros disponibles para la aleatoriedad
+        //for (int r= 0;r< (10*10);r++)
+        //{
+            //if (activatedX == 11)
+            //{
+            //    activatedX = 0;activatedY += 1;
+            //}
+            for (int i = 0; i < 10; i++)
+            {
+            Debug.Log("inicio");
+                //if (activatedX == i)
+                    for (int j = 0; j < 10; j++)
+                    {
+                        
+                        sorterOfPositions( i, j);
+                Debug.Log("activacion ");
+
+                    }
+            }
+            
+                     
+        //}
+
         
     }
-    void Update()
+    void sorterOfPositions( int i, int j)
     {
-        
-    }
-    // Separar por cuadrantes y grupos de cuadrantes según la posicion deseable
-    // Para ver la posicion de los cuadrantes convendría crear cuadricula tipo ajedrez
-    void sorterOfPositions()
-    {
-        switch (objectForTable.Length)
+        int boolean = Random.Range(0, 2);
+        switch (boolean)
         {
             case 0:
-                for (int i = 0; i < 8; i++)
-                {
-                    for (int j = 0; j < 8; j++)
-                    {
-                        squares[i, j] = Instantiate(objectForTable[0], new Vector3(i, j, 0), Quaternion.identity);
-                    }
-                }
+                Debug.Log("Slot empty (" + i + " , " + j + ")");
                 break;
             case 1:
-                break;
-            case 2:
+                //availablePos.Add(true, squares);
+                squares[i, j] = Instantiate(objectForTable[0], tablePos.position + new Vector3(i, 0, j), Quaternion.identity);
+                Debug.Log("Slot used (" + i + " , " + j + ")");
                 break;
         }
     }
