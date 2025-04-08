d1ee36959dd9b02a204777b867c2ca557c5bcd37
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-04-07 20:58:31 -0400

    Candado funcional mecanicamente hablando
    por ahora esta preparado solo como prueba, pero se le podría aplicar para usarlo con los controles VR

--- Assets/Fernando/script/NumberLocks.cs
+++ Assets/Fernando/script/NumberLocks.cs
@@ -3,19 +3,47 @@ using UnityEngine;
 public class NumberLocks : MonoBehaviour
 {// opcion por fisica, habria que colocar un detector para el numero que pasa en frente para que se detenga por movimiento
 // opcion por codigo, 1 interaccion = un mov de candado | por cada 36°
-    [SerializeField]GameObject numberLock1, numberLock2, numberLock3;
-    void Start()
+    public bool lockMove;
+    public NumberLocks otherNumberLock1,otherNumberLock2;
+    public GameObject lockAnimation;
+    [SerializeField]private int valueKey,value,unit;
+    private void OnEnable()
     {
-        
+        lockMove = false;
+        valueKey = Random.Range(0, 10);
+        value = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(unit==1)
+            if (Input.GetKeyUp(KeyCode.F))
+                rotationOfLock();
+
+        if(unit == 2)
+            if (Input.GetKeyUp(KeyCode.G))
+                rotationOfLock();
+
+        if (unit == 3)
+            if (Input.GetKeyUp(KeyCode.H))
+                rotationOfLock();
+
     }
-    void rotationOfLock(GameObject gameObject,GameObject hand)
-    {
-       // gameObject.transform.rotation += hand.transform.rotation;
+    public void rotationOfLock()
+    {   if(lockMove== false || otherNumberLock1.lockMove == false || otherNumberLock2.lockMove == false)
+        {
+            value += 1;
+            transform.Rotate(transform.rotation.x, transform.transform.rotation.y + 36, transform.rotation.z);
+        }
+
+        if (value >= 10)
+            value = 0;
+
+        if (value == valueKey)
+            lockMove = true;
+
+        if (lockMove == true && otherNumberLock1.lockMove == true && otherNumberLock2.lockMove == true)
+            lockAnimation.transform.localPosition = new(0, 0, 0);
     }
 }
