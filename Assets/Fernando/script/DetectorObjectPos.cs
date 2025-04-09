824bae85e467493935c77c6163e0ae743bd32437
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-04-09 09:41:16 -0400

    Mecánica de posición correcta de objetos, inicio de mecánica de linterna uv y del piano
    mecánica de posición funcional, linterna uv aun no funcional y piano le queda solo un par de correcciones

---
+++ Assets/Fernando/script/DetectorObjectPos.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DetectorObjectPos : MonoBehaviour
+{//lista/array de posiciones clave para resolver
+ //si la posicion tiene el objeto indicado entonces queda true bool
+ //cada objeto debe tener una ID para identificar en que pos va | cada objeto tendra su codigo con su bool
+    public ObjectPos[] objectPos;
+    public void orderSolution()
+    {
+        int totalCorrect = 0;
+        for (int i = 0; i < objectPos.Length; i++) 
+        {
+            if (objectPos[i].correctPos == true)
+            {
+                totalCorrect++;
+            }
+            else 
+            {
+
+            }
+        }
+        if (totalCorrect == objectPos.Length)  
+            Debug.Log("Correct placed order"); 
+    }
+}
---
+++ Assets/Fernando/script/LightUV.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LightUV : MonoBehaviour
+{
+    //al pasar por rayos (cono) el objeto se hará visible
+    void Update()
+    {
+
+    }
+}
