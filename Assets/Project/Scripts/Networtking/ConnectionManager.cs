164ae518c900a5ef52ab0f337aa68b8711065ea7
Fredy <fredy@hackmonkeys.com>
2025-04-10 08:56:57 -0500

    starting networking

---
+++ Assets/Project/Scripts/Networtking/ConnectionManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Fusion;
+using UnityEngine.SceneManagement;
+
+namespace CustomNetworking
+{
+    public class ConnectionManager : MonoBehaviour
+    {
+        [SerializeField] private NetworkRunner _networkRunner;
+        [SerializeField] private NetworkEvents _networkEvents;
+        [SerializeField] private NetworkObject[] _networkObjectsArray;
+
+        private void Awake()
+        {
+            _networkObjectsArray = GetNetworkObjects();
+            _networkRunner = GetComponent<NetworkRunner>();
+            _networkEvents = GetComponent<NetworkEvents>();
+
+            _networkEvents.OnConnectedToServer.AddListener(RegisterNetworkObjects);
+        }
+
+        private void Start()
+        {
+            _networkRunner.StartGame(new StartGameArgs()
+            {
+                GameMode = GameMode.Shared,
+                SessionName = SceneManager.GetActiveScene().name,
+                // Es necesario añadir el SceneManager para gestionar escenas en Fusion
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            });
+        }
+
+        private void OnDestroy() => _networkEvents.OnConnectedToServer.RemoveListener(RegisterNetworkObjects);
+
+        private NetworkObject[] GetNetworkObjects() => FindObjectsByType<NetworkObject>(FindObjectsSortMode.None);
+
+        private void RegisterNetworkObjects(NetworkRunner runner) =>
+            runner.RegisterSceneObjects(SceneRef.FromIndex(0), _networkObjectsArray);
+    }
+}
