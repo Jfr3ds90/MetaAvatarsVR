a3a62336629fce1778cff52de32c0c3ef95ef93d
Fredy <fredy@hackmonkeys.com>
2025-04-15 08:07:04 -0500

    test multiplayer

--- Assets/Project/Scripts/Networtking/ConnectionManager.cs
+++ Assets/Project/Scripts/Networtking/ConnectionManager.cs
@@ -2,39 +2,37 @@ using UnityEngine;
 using Fusion;
 using UnityEngine.SceneManagement;
 
-namespace CustomNetworking
+
+public class ConnectionManager : MonoBehaviour
 {
-    public class ConnectionManager : MonoBehaviour
+    [SerializeField] private NetworkRunner _networkRunner;
+    [SerializeField] private NetworkEvents _networkEvents;
+    [SerializeField] private NetworkObject[] _networkObjectsArray;
+
+    private void Awake()
     {
-        [SerializeField] private NetworkRunner _networkRunner;
-        [SerializeField] private NetworkEvents _networkEvents;
-        [SerializeField] private NetworkObject[] _networkObjectsArray;
+        _networkObjectsArray = GetNetworkObjects();
+        _networkRunner = GetComponent<NetworkRunner>();
+        _networkEvents = GetComponent<NetworkEvents>();
 
-        private void Awake()
-        {
-            _networkObjectsArray = GetNetworkObjects();
-            _networkRunner = GetComponent<NetworkRunner>();
-            _networkEvents = GetComponent<NetworkEvents>();
-
-            _networkEvents.OnConnectedToServer.AddListener(RegisterNetworkObjects);
-        }
-
-        private void Start()
-        {
-            _networkRunner.StartGame(new StartGameArgs()
-            {
-                GameMode = GameMode.Shared,
-                SessionName = SceneManager.GetActiveScene().name,
-                // Es necesario añadir el SceneManager para gestionar escenas en Fusion
-                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
-            });
-        }
-
-        private void OnDestroy() => _networkEvents.OnConnectedToServer.RemoveListener(RegisterNetworkObjects);
-
-        private NetworkObject[] GetNetworkObjects() => FindObjectsByType<NetworkObject>(FindObjectsSortMode.None);
-
-        private void RegisterNetworkObjects(NetworkRunner runner) =>
-            runner.RegisterSceneObjects(SceneRef.FromIndex(0), _networkObjectsArray);
+        _networkEvents.OnConnectedToServer.AddListener(RegisterNetworkObjects);
     }
+
+    private void Start()
+    {
+        _networkRunner.StartGame(new StartGameArgs()
+        {
+            GameMode = GameMode.Shared,
+            SessionName = SceneManager.GetActiveScene().name,
+            // Es necesario añadir el SceneManager para gestionar escenas en Fusion
+            SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+        });
+    }
+
+    private void OnDestroy() => _networkEvents.OnConnectedToServer.RemoveListener(RegisterNetworkObjects);
+
+    private NetworkObject[] GetNetworkObjects() => FindObjectsByType<NetworkObject>(FindObjectsSortMode.None);
+
+    private void RegisterNetworkObjects(NetworkRunner runner) =>
+        runner.RegisterSceneObjects(SceneRef.FromIndex(0), _networkObjectsArray);
 }
