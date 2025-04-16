f024f967b6614e75ca6971e248e96ec3e2e0c101
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-04-16 18:22:10 -0400

    Mejoras linterna
    solo el color magenta detecta el material secreto

--- Assets/Mecanicas/script/RevealingEffect.cs
+++ Assets/Mecanicas/script/RevealingEffect.cs
@@ -1,13 +1,16 @@
-using System.Collections;
+
 using UnityEngine;
-using UnityEngine.Experimental.GlobalIllumination;
 
 public class RevealingEffect : MonoBehaviour
 {
     bool OnOff = false;
+    
+    MeshRenderer LastMeshRenderer = null;
     private void Update()
     {
+        if (OnOff== true)
         Appear();
+
         if (Input.GetKeyUp(KeyCode.Q))
         changeLight();
         if (Input.GetKeyUp(KeyCode.R)) 
@@ -20,9 +23,11 @@ public class RevealingEffect : MonoBehaviour
         
         Light light;
         light = GetComponent<Light>();
+
+        if (GetComponentInParent<MeshRenderer>().materials[1].color == Color.magenta)
         for (float i = 0; i < 15; i++)
         {
-           // var currentPointPosition = Quaternion.AngleAxis(i, transform.forward) * transform.forward;
+            var currentPointPosition = Quaternion.AngleAxis(i, transform.forward) * transform.forward;
             var currentPointPositionRight = Quaternion.AngleAxis(i, transform.right) * transform.forward;
             var currentPointPositionUp = Quaternion.AngleAxis(i, transform.up) * transform.forward;
             var currentPointPositionLeft = Quaternion.AngleAxis(i, -transform.right) * transform.forward;
@@ -34,26 +39,57 @@ public class RevealingEffect : MonoBehaviour
                 || Physics.Raycast(transform.position, currentPointPositionLeft + currentPointPositionDown, out hit, light.range))
                 {
                 MeshRenderer meshRenderer = hit.collider.GetComponent<MeshRenderer>();
-                    if (hit.collider.GetComponent<MeshRenderer>().material.name == "M_PistaUV")
-                    meshRenderer.material.SetFloat("_Aparicion", meshRenderer.material.GetFloat("_Aparicion")+light.intensity);
-                    else
-                    {
-                    meshRenderer.material.SetFloat("_Aparicion", -light.intensity);
-                    if(meshRenderer.material.GetFloat("_Aparicion") <= 0)
-                        meshRenderer.material.SetFloat("_Aparicion", 0) ;
-                    }
+                //Debug.Log("el material es "+ hit.collider.GetComponent<MeshRenderer>().material.name);
+                if (hit.collider.GetComponent<MeshRenderer>().material.name == "M_PistaUV" || hit.collider.GetComponent<MeshRenderer>().material.name == "M_PistaUV (Instance)")
+                { 
+                    meshRenderer.material.SetFloat("_Aparicion", meshRenderer.material.GetFloat("_Aparicion") + light.intensity);
+                        
+                        meshRenderer.material.SetVector("_PosicionLuz", hit.point - hit.transform.position);// obtiene la posicion del rayo - posicion del objeto
+                        Debug.Log(hit.point-hit.transform.position);
+                    if (meshRenderer.material.GetFloat("_Aparicion") >= 1)
+                        meshRenderer.material.SetFloat("_Aparicion", 1);
+
+                    LastMeshRenderer = meshRenderer;
                 }
-       
+                else if (hit.collider.GetComponent<MeshRenderer>().material.name != "M_PistaUV" && hit.collider.GetComponent<MeshRenderer>().material.name != "M_PistaUV (Instance)")
+                {
+                    //Debug.Log("no choca y es "+LastMeshRenderer);
+
+                    if(LastMeshRenderer!=null)
+                    {
+                        LastMeshRenderer.material.SetFloat("_Aparicion", -light.intensity);
+                        if (LastMeshRenderer.material.GetFloat("_Aparicion") <= 0)
+                            LastMeshRenderer.material.SetFloat("_Aparicion", 0);
+                    }
+
                     
+                }
+            
             }
-        
+
+            
+            //Debug.DrawRay(transform.position, currentPointPositionRight + currentPointPositionUp, Color.green,light.range);
+            //Debug.DrawRay(transform.position, currentPointPositionLeft + currentPointPositionUp, Color.green, light.range);
+            //Debug.DrawRay(transform.position, currentPointPositionRight + currentPointPositionDown, Color.green, light.range);
+            //Debug.DrawRay(transform.position, currentPointPositionLeft + currentPointPositionDown, Color.green, light.range);
+
+
+        }
+        else if (LastMeshRenderer != null)
+        {
+            LastMeshRenderer.material.SetFloat("_Aparicion", -light.intensity);
+            if (LastMeshRenderer.material.GetFloat("_Aparicion") <= 0)
+                LastMeshRenderer.material.SetFloat("_Aparicion", 0);
+        }
+
     }
     public void changeLight() //cambio de color de linterna
     {
-        if (OnOff == true)
-        {
-            MeshRenderer meshRenderer;
+        MeshRenderer meshRenderer;
             meshRenderer = GetComponentInParent<MeshRenderer>();
+        Debug.Log(meshRenderer);
+        if (OnOff == true)
+        {  
             Debug.Log(meshRenderer.materials[1].color + " es el color");
             if (meshRenderer.materials[1].color == Color.magenta || meshRenderer.materials[1].color == Color.black)
             {
@@ -74,7 +110,7 @@ public class RevealingEffect : MonoBehaviour
     public void OnOffLight()
     {
         MeshRenderer meshRenderer = GetComponentInParent<MeshRenderer>();
-        if (meshRenderer.materials[1].color != Color.black)
+        if (OnOff == true)
         {
             meshRenderer.materials[1].color = Color.black;
             meshRenderer.materials[1].SetColor("_EmissionColor", Color.black);
