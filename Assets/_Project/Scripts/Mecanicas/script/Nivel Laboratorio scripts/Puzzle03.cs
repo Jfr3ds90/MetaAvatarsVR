0d6544104d038cb7565fd38f7cd15482ecd19650
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-08-12 18:11:31 -0400

    avance aleatoriedad funcional puzzle 3

--- Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle03.cs
+++ Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle03.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Puzzle03 : MonoBehaviour
 {
     public Sprite[] level1, level2, level3, level4, level5, level6, level7, level8, level9;
-    public Sprite empty;
     public GameObject[] squarePos;
     public GameObject pos, instrctions;
-    int currentLevel = 0;
+    int currentLevel = 0,empty;
     bool trueImage = true;
     List<Sprite> LSprite= new List<Sprite>();
     List<int> lis = new List<int>();
+   public Vector2 selectPos = new Vector2(0, 0),emptyPos;//maximo 3,-3
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.LeftArrow)) LMove();
@@ -28,6 +28,7 @@ public class Puzzle03 : MonoBehaviour
             else if(trueImage == false && pos.transform.localPosition.x- 0.325f > -0.815f)
             {
                 pos.transform.localPosition -= new Vector3(0.325f,0);
+                selectPos.x -= 1;
             }
         }
         else if (instrctions.activeSelf == true)
@@ -42,6 +43,7 @@ public class Puzzle03 : MonoBehaviour
             else if (trueImage == false && pos.transform.localPosition.x+ 0.325f < 0.485f)
             {
                 pos.transform.localPosition += new Vector3(0.325f, 0);
+                selectPos.x += 1;
             }
         }
         else if (instrctions.activeSelf == true)
@@ -56,6 +58,7 @@ public class Puzzle03 : MonoBehaviour
             else if (trueImage == false && pos.transform.localPosition.y+0.325f < 0.5f)
             {
                 pos.transform.localPosition += new Vector3(0, 0.325f);
+                selectPos.y += 1;
             }
         }
         else if (instrctions.activeSelf == true)
@@ -70,6 +73,7 @@ public class Puzzle03 : MonoBehaviour
             else if (trueImage == false && pos.transform.localPosition.y-0.325f > -0.5f) 
             {
                 pos.transform.localPosition -= new Vector3(0, 0.325f);
+                selectPos.y -= 1;
             }
         }
         else if (instrctions.activeSelf == true)
@@ -116,51 +120,116 @@ public class Puzzle03 : MonoBehaviour
     }
     void RandomSprite()
     {
-       
+        int x, y;
         switch(currentLevel)
         {
             case 0:
                 foreach (Sprite s in level1)
-                    LSprite.Add(s); Debug.LogWarning(LSprite[0]+ " es el sprite 0");
-            //   LSprite.RemoveAt( Random.Range(0,LSprite.Count));//el vacio
-                for (int i = 0; i< squarePos.Length;i++)
-                {
-                        RS(i);                    
-                }
+                    LSprite.Add(s);
+                for (int i = 0; i< squarePos.Length;i++)               
+                        RS(i);                                    
                 lis.Clear();
-                squarePos[Random.Range(0, squarePos.Length)].GetComponent<SpriteRenderer>().sprite = empty;
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0,0,0,255);
+                 y = empty/-4;
+                 x = empty%4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x,y);
                 break;
             case 1:
                 foreach (Sprite s in level2)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                 y = empty / -4;
+                 x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 2:
                 foreach (Sprite s in level3)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 3:
                 foreach (Sprite s in level4)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 4:
                 foreach (Sprite s in level5)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 5:
                 foreach (Sprite s in level6)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 6:
                 foreach (Sprite s in level7)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 7:
                 foreach (Sprite s in level8)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
             case 8:
-                foreach (Sprite s in level1)
+                foreach (Sprite s in level9)
                     LSprite.Add(s);
+                for (int i = 0; i < squarePos.Length; i++)
+                    RS(i);
+                lis.Clear();
+                empty = Random.Range(0, squarePos.Length);
+                squarePos[empty].GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 255);
+                y = empty / -4;
+                x = empty % 4;//0 - 3 = 0 | 4 - 7 = 1 | 8 - 11 = 2 | 12 - 15 = 3
+                emptyPos = new Vector2(x, y);
                 break;
         }
         LSprite.Clear();
@@ -171,12 +240,20 @@ public class Puzzle03 : MonoBehaviour
         if (!lis.Contains(v))
         {
             lis.Add(v);
-            squarePos[i].GetComponent<SpriteRenderer>().sprite = level1[v];
-          //  LSprite.RemoveAt(v);
+            switch (currentLevel)
+            {
+                case 0:squarePos[i].GetComponent<SpriteRenderer>().sprite = level1[v];break;
+                case 1:squarePos[i].GetComponent<SpriteRenderer>().sprite = level2[v];break;
+                case 2:squarePos[i].GetComponent<SpriteRenderer>().sprite = level3[v];break;
+                case 3:squarePos[i].GetComponent<SpriteRenderer>().sprite = level4[v];break;
+                case 4:squarePos[i].GetComponent<SpriteRenderer>().sprite = level5[v];break;
+                case 5:squarePos[i].GetComponent<SpriteRenderer>().sprite = level6[v];break;
+                case 6:squarePos[i].GetComponent<SpriteRenderer>().sprite = level7[v];break;
+                case 7:squarePos[i].GetComponent<SpriteRenderer>().sprite = level8[v];break;
+                case 8:squarePos[i].GetComponent<SpriteRenderer>().sprite = level9[v];break;
+            }
         }
         else if (lis.Contains(v))
-            RS(i);
-
-        
+            RS(i);   
     }
 }
