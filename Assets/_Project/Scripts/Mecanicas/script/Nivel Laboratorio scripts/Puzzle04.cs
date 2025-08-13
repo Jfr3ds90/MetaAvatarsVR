dafbd52dbed20eb0dfec2e43baf74486e5b2a9bd
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-08-13 18:07:35 -0400

    avance puzzle 4

--- Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle04.cs
+++ Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle04.cs
@@ -3,5 +3,41 @@ using UnityEngine;
 public class Puzzle04 : MonoBehaviour
 {
     public Sprite[] Kimbo,Laberynth;
-    public GameObject pos, instrctions;
+    public GameObject pos, instrctions,kimbo;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) LMove(false);
+        if (Input.GetKeyUp(KeyCode.RightArrow)) RMove(false);
+        if (Input.GetKeyUp(KeyCode.UpArrow)) UMove(false);
+        if (Input.GetKeyUp(KeyCode.DownArrow)) DMove(false);
+    }
+    public void LMove(bool mov)
+    {
+      var anim = kimbo.GetComponent<Animator>();
+        anim.SetBool("Left", mov);
+        anim.SetBool("Walk", mov);
+    }
+    public void RMove(bool mov)
+    {
+        var anim = kimbo.GetComponent<Animator>();
+        anim.SetBool("Right", mov);
+        anim.SetBool("Walk", mov);
+    }
+    public void DMove(bool mov)
+    {
+        var anim = kimbo.GetComponent<Animator>();
+        anim.SetBool("Down", mov);
+        anim.SetBool("Walk", mov);
+    }
+    public void UMove(bool mov)
+    {
+        var anim = kimbo.GetComponent<Animator>();
+        anim.SetBool("Up", mov);
+        anim.SetBool("Walk", mov);
+    }
+    public void GButton(bool mov)
+    {
+        anim.SetBool("Walk", mov);
+    }
 }
