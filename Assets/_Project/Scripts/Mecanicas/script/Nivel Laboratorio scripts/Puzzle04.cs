4ca82f285c9f217760993823f8eadff9ab11e92d
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-08-18 14:04:27 -0400

    movimiento kimbo

--- Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle04.cs
+++ Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle04.cs
@@ -7,34 +7,51 @@ public class Puzzle04 : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow)) LMove(true); else LMove(false);
-       // if (Input.GetKey(KeyCode.RightArrow)) RMove(true); else RMove(false);
-       // if (Input.GetKey(KeyCode.UpArrow)) UMove(true); else UMove(false);
-       // if (Input.GetKey(KeyCode.DownArrow)) DMove(true); else DMove(false);
+        //if (Input.GetKey(KeyCode.LeftArrow)) LMove(true); else LMove(false);
+        //if (Input.GetKey(KeyCode.RightArrow)) RMove(true); else RMove(false);
+        //if (Input.GetKey(KeyCode.UpArrow)) UMove(true); else UMove(false);
+        //if (Input.GetKey(KeyCode.DownArrow)) DMove(true); else DMove(false);
     }
     public void LMove(bool mov)
     {
       var anim = kimbo.GetComponent<Animator>();
-        anim.SetFloat("XValue", -1);
         anim.SetBool("Walk", mov);
+        if(mov==false)
+        {
+            anim.SetFloat("XValue", -1);
+            kimbo.transform.position += new Vector3(Time.deltaTime * 0.1f, 0);
+        }
     }
     public void RMove(bool mov)
     {
         var anim = kimbo.GetComponent<Animator>();
-        anim.SetFloat("XValue", 1);
         anim.SetBool("Walk", mov);
+        if (mov == false)
+        { 
+            anim.SetFloat("XValue", 1);
+            kimbo.transform.position -= new Vector3(Time.deltaTime * 0.1f, 0);
+        }
+        
     }
     public void DMove(bool mov)
     {
         var anim = kimbo.GetComponent<Animator>();
-        anim.SetFloat("YValue", -1);
         anim.SetBool("Walk", mov);
+        if (mov == false)
+        {
+            anim.SetFloat("YValue", -1);
+            kimbo.transform.position += new Vector3(0, Time.deltaTime * 0.1f);
+        }
     }
     public void UMove(bool mov)
     {
         var anim = kimbo.GetComponent<Animator>();
-        anim.SetFloat("YValue", 1);
         anim.SetBool("Walk", mov);
+        if (mov == false)
+        {
+            anim.SetFloat("YValue", 1);
+            kimbo.transform.position -= new Vector3(0, Time.deltaTime * 0.1f);
+        }
     }
     public void GButton(bool mov)
     {
