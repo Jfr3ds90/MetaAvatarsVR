bb44762a865983f42a3ae3968c02a8bd2028f295
FernandoHackmonkeys <fernando@hackmonkeys.com>
2025-08-19 17:47:04 -0400

    avance Puzzle 2

--- Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle02.cs
+++ Assets/_Project/Scripts/Mecanicas/script/Nivel Laboratorio scripts/Puzzle02.cs
@@ -7,19 +7,40 @@ public class Puzzle02 : MonoBehaviour
     public GameObject[] Empty,Full;
     public GameObject pos, instrctions;
     public Vector2 selectPos = new Vector2(0, 0);
-    Dictionary<GameObject,Vector2> Coord = new Dictionary<GameObject, Vector2>();
+    Dictionary<GameObject,Vector2> Coord = new Dictionary<GameObject, Vector2>();//segun el gameobject, es el mismo en la lista de int acorde al orden *tecnicamente*
+    List<int> emptyPlaced = new List<int>();
+    public int posObj;
     //0.225
     private void Awake()
     {
         for (int i = 0; i < Empty.Length; i++)
-        Coord.Add(Empty[i],new Vector2(0,0));
+        {
+            int value = Random.Range(0, empty.Length);
+            Coord.Add(Empty[i], new Vector2(0, 0));
+            Empty[i].GetComponent<SpriteRenderer>().sprite = empty[value];
+            Full[i].GetComponent<SpriteRenderer>().sprite = full[value];
+            emptyPlaced.Add(value);
+          //  Empty[i].SetActive(true);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) LMove();
+        if (Input.GetKeyUp(KeyCode.RightArrow)) RMove();
+        if (Input.GetKeyUp(KeyCode.UpArrow)) UMove();
+        if (Input.GetKeyUp(KeyCode.DownArrow)) DMove();
+        if (Input.GetKeyUp(KeyCode.Z)) GButton(); 
     }
     public void LMove()
     {
         if (instrctions.activeSelf == false)
         {
-            pos.transform.localPosition -= new Vector3(0.225f,0);
-            selectPos.x -= 1;
+            if(selectPos.x>0)
+            {
+                pos.transform.localPosition -= new Vector3(0.225f,0);
+                selectPos.x -= 1;
+                posObj -= 1;
+            }
         }
         else if (instrctions.activeSelf == true)
             instrctions.SetActive(false);
@@ -28,8 +49,12 @@ public class Puzzle02 : MonoBehaviour
     {
         if (instrctions.activeSelf == false)
         {
-            pos.transform.localPosition += new Vector3(0.225f, 0);
-            selectPos.x += 1;
+            if (selectPos.x  < 5)
+            {
+                pos.transform.localPosition += new Vector3(0.225f, 0);
+                selectPos.x += 1;
+                posObj += 1;
+            }
         }
         else if (instrctions.activeSelf == true)
             instrctions.SetActive(false);
@@ -38,8 +63,12 @@ public class Puzzle02 : MonoBehaviour
     {
         if (instrctions.activeSelf == false)
         {
-            pos.transform.localPosition += new Vector3(0, 0.225f);
-            selectPos.y += 1;
+            if (selectPos.y < 0)
+            {
+                pos.transform.localPosition += new Vector3(0, 0.225f);
+                selectPos.y += 1;
+                posObj -= 6;
+            }
         }
         else if (instrctions.activeSelf == true)
             instrctions.SetActive(false);
@@ -48,10 +77,18 @@ public class Puzzle02 : MonoBehaviour
     {
         if (instrctions.activeSelf == false)
         {
-            pos.transform.localPosition -= new Vector3(0, 0.225f);
-            selectPos.y -= 1;
+            if (selectPos.y > -5)
+            {
+                pos.transform.localPosition -= new Vector3(0, 0.225f);
+                selectPos.y -= 1;
+                posObj += 6;
+            }
         }
         else if (instrctions.activeSelf == true)
             instrctions.SetActive(false);
+    }
+    public void GButton()
+    {
+       Debug.LogWarning("El sprite utilizado es "+emptyPlaced.IndexOf(posObj));
     }
 }
